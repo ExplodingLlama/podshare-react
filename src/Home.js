42f2ffdd4967c87c6a67ff2fc2e7c945a69c4cdb
Ratandeep Singh <ratan14@gmail.com>
2019-02-04 01:14:48 +0530

    added a front page

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,40 @@
 import React from "react";
 import Parser from "rss-parser";
+import axios from "axios";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       parser: new Parser(),
-      feed: { items: [] }
+      podlinks: []
     };
   }
   componentDidMount() {
-    this.state.parser
-      .parseURL("https://verybadwizards.fireside.fm/rss")
-      .then(feed => {
-        this.setState({ feed });
-      });
+    // this.state.parser
+    //   .parseURL("https://verybadwizards.fireside.fm/rss")
+    //   .then(feed => {
+    //     this.setState({ feed });
+    //   });
+    axios.get("/getAllLinks").then(response => {
+      this.setState({ podlinks: response.data });
+    });
   }
   render() {
     return (
-      <React.Fragment>
-        We have {this.state.feed.items.length} items for you to enjoy!
-      </React.Fragment>
+      <div style={{ margin: "20px" }}>
+        <div style={{ fontSize: "30px", padding: "10px" }}>
+          Pod Linker lets you share your favourite podcast clips with your
+          friends
+        </div>
+        {this.state.podlinks.map(link => {
+          return (
+            <div style={{ padding: "10px" }}>
+              <a href={"/" + link.id}>{link.title}</a>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
